d2676d14fa2185956b33c668e00965e3c94ccb5d
wmas1123 <61249615+wmas1123@users.noreply.github.com>
2020-05-22 18:36:34 -0700

    Create Program3.java

---
+++ Program 3/Program3.java
@@ -0,0 +1,11 @@
+/***********************************************************************************
+ * Name William Mastronardi CSC205 25993 This program will create an array of
+ * states and their information including the year they were founded and joined
+ * the United States, their state bird, their population. The information
+ * regarding the states will be read from a file and placed into an array of
+ * states records.
+ * 
+ ***********************************************************************************/
+public class Program3 {
+
+}
